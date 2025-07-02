f91a996db7fb71f4160ccaacc7ad1ee30199b516
prima ramadhani putra <primaramadhanip@gmail.com>
2025-07-02 16:18:56 +0700

    fix long content

--- dev/index.tsx
+++ dev/index.tsx
@@ -14,7 +14,19 @@ const App = () => (
     <h1>Testing Accordion Component</h1>
     <Accordion>
       <AccordionItem>
-        <AccordionButton>button 1</AccordionButton>
+        <AccordionButton>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+          voluptatum nulla dignissimos quia eius quo, ut odit adipisci impedit
+          vel quos? Doloremque enim odit ipsa, repellat, aliquam modi aperiam
+          mollitia pariatur soluta excepturi temporibus. Quia quasi
+          voluptatibus, laborum obcaecati nesciunt facilis totam eveniet, eos
+          maxime dolore eaque, ab vel nulla minus vero! Eos quam iusto incidunt
+          delectus fugit ducimus, nobis numquam illum ut magnam earum deserunt
+          asperiores voluptate fuga eum. Sapiente repudiandae molestias et
+          voluptas ea quidem at excepturi eum recusandae, porro nemo hic ratione
+          aliquam provident eius ipsam dignissimos ab placeat accusantium
+          dolorum! Animi et aut pariatur ratione iusto.
+        </AccordionButton>
         <AccordionPanel>Panel 1</AccordionPanel>
       </AccordionItem>
       <AccordionItem>
--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -116,22 +116,24 @@ export const AccordionButton = ({
       {children}
       {/* <div>{children}</div> */}
       {expand ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="25"
-          viewBox="0 0 24 25"
-          fill="none"
-          className={"hudoro-accordion-arrow"}
-        >
-          <path
-            d="M4 9.5L12 17.5L20 9.5"
-            stroke="#111827"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+            className={"hudoro-accordion-arrow"}
+          >
+            <path
+              d="M4 9.5L12 17.5L20 9.5"
+              stroke="#111827"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
       ) : null}
     </button>
   );
